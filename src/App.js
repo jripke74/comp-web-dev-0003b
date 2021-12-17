c8fe03906a8a24a01455e6247afe5848e054daed
jripke74@gmail.com <jripke74@gmail.com>
2021-12-17 08:48:41 -0800

    add face detection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
+
+console.log(Clarifai);
+
+const app = new Clarifai.App({
+  apiKey: "f55ad295b40a414c86e2bc6a11eaba5a",
+});
 
 const particlesOptions = {
   particles: {
@@ -13,12 +20,38 @@ const particlesOptions = {
       density: {
         enable: true,
         value_area: 1100,
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://blog.hootsuite.com/wp-content/uploads/2021/07/free-stock-photos-03-scaled.jpeg"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {}
+      );
+  };
+
   render() {
     return (
       <div className="App">
@@ -26,7 +59,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
       </div>
     );
   }
